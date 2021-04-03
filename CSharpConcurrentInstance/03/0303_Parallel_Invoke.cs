804fed0bfdfb2fc05f5ce4f1549fa9cb23afe33a
常祈 <42560244+dmer175@users.noreply.github.com>
2021-04-03 22:59:32 +0800

    0303 并行调用

---
+++ CSharpConcurrentInstance/03/0303_Parallel_Invoke.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcurrentInstance._03
+{
+    /*
+     * Q:需要并行调用一批方法，并且这些方法（大部分）是互相独立的。
+     * A:Parallel.Invoke()
+     */
+    public class _0303_Parallel_Invoke
+    {
+        public static void ProcessArray(double [] array)
+        {
+            //支持token取消
+            //invoke适用于简单无返回并行
+            Parallel.Invoke(() => ProcessPartialArray(array, 0, array.Length / 2),
+                () => ProcessPartialArray(array, array.Length / 2, array.Length));
+        }
+
+        private static void ProcessPartialArray(double[] array, int begin, int end)
+        {
+            //some Intensive processing
+        }
+    }
+}
